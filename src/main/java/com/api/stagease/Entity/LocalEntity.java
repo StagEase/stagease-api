ddbf8bbac6e1a4c185246e405ed895f2d3093d14
gustavoemf <gustavoeduardomf@gmail.com>
2023-08-26 11:14:38 -0400

    Improve: Atributos + Annotations + Constructors LocalEntity

--- src/main/java/com/api/stagease/Entity/LocalEntity.java
+++ src/main/java/com/api/stagease/Entity/LocalEntity.java
@@ -1,4 +1,34 @@
 package com.api.stagease.Entity;
 
-public class LocalEntity {
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "local", schema = "stagease")
+public class LocalEntity extends AbstractEntity{
+    @Column(length = 50, nullable = false)
+    private String nome;
+    @Column(length = 50)
+    private String email;
+    @ManyToMany
+    @JoinTable(
+            name = "local_curso",
+            joinColumns = @JoinColumn(name = "local_id"),
+            inverseJoinColumns = @JoinColumn(name = "curso_id")
+    )
+    private List<CursoEntity> cursos = new ArrayList<>();
+
+    public LocalEntity() {}
+
+    public LocalEntity(String nome, String email, List<CursoEntity> cursos) {
+        this.nome = nome;
+        this.email = email;
+        this.cursos = cursos;
+    }
 }
