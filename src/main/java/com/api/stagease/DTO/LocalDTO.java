1532015e28f7723c8d4634a08e0991232cc06968
gustavoemf <gustavoeduardomf@gmail.com>
2023-08-26 11:05:34 -0400

    Improve: Getter & Setters + Constructors + Atributos LocalDTO

--- src/main/java/com/api/stagease/DTO/LocalDTO.java
+++ src/main/java/com/api/stagease/DTO/LocalDTO.java
@@ -1,4 +1,9 @@
 package com.api.stagease.DTO;
 
+import com.api.stagease.Entity.CursoEntity;
+
 public class LocalDTO {
+    private String nome;
+    private String email;
+    private CursoEntity cursos;
 }
