6e27fd281df5a9ca3bb2a6a8e26cd41ee09793d9
gustavoemf <gustavoeduardomf@gmail.com>
2023-08-26 11:23:46 -0400

    Improve: Atributos + Annotations + Constructors EstagiarioEntity

--- src/main/java/com/api/stagease/Entity/EstagiarioEntity.java
+++ src/main/java/com/api/stagease/Entity/EstagiarioEntity.java
@@ -1,4 +1,29 @@
 package com.api.stagease.Entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "local", schema = "stagease")
 public class EstagiarioEntity extends AbstractEntity{
+    @Column(length = 50, nullable = false)
+    private String nome;
+    @Column(length = 50, nullable = false)
+    private String curso;
+    @Column(length = 50, nullable = false)
+    private String periodo;
+    @Column(length = 50, nullable = false)
+    private String ie;
+
+    public EstagiarioEntity() {}
+
+    public EstagiarioEntity(String nome, String curso, String periodo, String ie) {
+        this.nome = nome;
+        this.curso = curso;
+        this.periodo = periodo;
+        this.ie = ie;
+    }
 }
