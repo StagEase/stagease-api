99356fa3785ad2f73d32053a943c492540f28cf6
gustavoemf <gustavoeduardomf@gmail.com>
2023-08-26 11:06:22 -0400

    Refactor: Correção do commit anterior

--- src/main/java/com/api/stagease/DTO/LocalDTO.java
+++ src/main/java/com/api/stagease/DTO/LocalDTO.java
@@ -1,9 +1,21 @@
 package com.api.stagease.DTO;
 
 import com.api.stagease.Entity.CursoEntity;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class LocalDTO {
     private String nome;
     private String email;
     private CursoEntity cursos;
+
+    public LocalDTO() {}
+
+    public LocalDTO(String nome, String email, CursoEntity cursos) {
+        this.nome = nome;
+        this.email = email;
+        this.cursos = cursos;
+    }
 }
