f5e7e7582a29dd2915fedad83f43a8792516d53c
gustavoemf <gustavoeduardomf@gmail.com>
2023-08-29 18:22:18 -0400

    Fix: correção de retorno das DTOs na LocalService & LocalController

--- src/main/java/com/api/stagease/Service/LocalService.java
+++ src/main/java/com/api/stagease/Service/LocalService.java
@@ -4,6 +4,7 @@ import com.api.stagease.DTO.LocalDTO;
 import com.api.stagease.Entity.LocalEntity;
 import com.api.stagease.Repository.LocalRepository;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,22 +17,26 @@ public class LocalService {
     private ModelMapper modelMapper;
 
     @Transactional
-    public LocalEntity create(LocalDTO dto) {
+    public LocalDTO create(LocalDTO dto) {
         if (dto.getId() != null) {
             throw new RuntimeException("Não insira o id manualmente");
         }
         LocalEntity entity = modelMapper.map(dto, LocalEntity.class);
-        return repository.save(entity);
+        LocalDTO local = new LocalDTO();
+        BeanUtils.copyProperties(repository.save(entity), local);
+        return local;
     }
 
     @Transactional
-    public LocalEntity update(Long id, LocalDTO dto) {
+    public LocalDTO update(Long id, LocalDTO dto) {
         LocalEntity banco = this.repository.findById(id).orElse(null);
         if (!banco.getId().equals(dto.getId())) {
             throw new RuntimeException("Não foi possivel encontrar o registro informado");
         }
         modelMapper.map(dto, banco);
-        return repository.save(banco);
+        LocalDTO local = new LocalDTO();
+        BeanUtils.copyProperties(repository.save(banco), local);
+        return local;
     }
 
     @Transactional
