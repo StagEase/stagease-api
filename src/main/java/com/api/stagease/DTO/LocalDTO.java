c2b0ba452654373faab18ed6a98ebad9d1ff4c20
gustavoemf <gustavoeduardomf@gmail.com>
2023-08-29 11:40:20 -0400

    Refactor: Utilizando annotations para constructors LocalDTO

--- src/main/java/com/api/stagease/DTO/LocalDTO.java
+++ src/main/java/com/api/stagease/DTO/LocalDTO.java
@@ -1,25 +1,20 @@
 package com.api.stagease.DTO;
 
 import com.api.stagease.Entity.CursoEntity;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.List;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class LocalDTO {
     private Long id;
     private String nomeLocal;
     private String email;
     private List<CursoEntity> ocorrencias;
-
-    public LocalDTO() {}
-
-    public LocalDTO(Long id, String nomeLocal, String email, List<CursoEntity> ocorrencias) {
-        this.id = id;
-        this.nomeLocal = nomeLocal;
-        this.email = email;
-        this.ocorrencias = ocorrencias;
-    }
 }
