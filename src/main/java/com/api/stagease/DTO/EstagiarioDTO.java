05b5cb9906a995116e86d5f5e117fa4390b38957
gustavoemf <gustavoeduardomf@gmail.com>
2023-08-29 11:40:04 -0400

    Refactor: Utilizando annotations para constructors EstagiarioDTO

--- src/main/java/com/api/stagease/DTO/EstagiarioDTO.java
+++ src/main/java/com/api/stagease/DTO/EstagiarioDTO.java
@@ -1,25 +1,19 @@
 package com.api.stagease.DTO;
 
 import com.api.stagease.Entity.CursoEntity;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class EstagiarioDTO {
     private Long id;
     private String nomeEstagiario;
     private CursoEntity curso;
     private String periodo;
     private String instituicaoDeEnsino;
-
-    public EstagiarioDTO() {}
-
-    public EstagiarioDTO(Long id, String nomeEstagiario, CursoEntity curso, String periodo, String instituicaoDeEnsino) {
-        this.id = id;
-        this.nomeEstagiario = nomeEstagiario;
-        this.curso = curso;
-        this.periodo = periodo;
-        this.instituicaoDeEnsino = instituicaoDeEnsino;
-    }
 }
