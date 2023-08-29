5f9128011c0434c1e3fcb2ef6071ecfe2a537655
marcelonv011 <marcelonv011@gmail.com>
2023-08-29 11:43:58 -0300

    solicitacao dto alterando

--- src/main/java/com/api/stagease/DTO/SolicitacaoDTO.java
+++ src/main/java/com/api/stagease/DTO/SolicitacaoDTO.java
@@ -1,10 +1,20 @@
 package com.api.stagease.DTO;
 
+import com.api.stagease.Entity.*;
+import com.api.stagease.Entity.Enum.Situacao;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.List;
 
 @Getter
 @Setter
 public class SolicitacaoDTO {
-
+    private Long id;
+    private LocalEntity local;
+    private EstagiarioEntity estagiario;
+    private SupervisorEntity supervisor;
+    private DuracaoEntity duracao;
+    private List<Situacao> situacoes;
+    private ExpedienteEntity expediente;
 }
