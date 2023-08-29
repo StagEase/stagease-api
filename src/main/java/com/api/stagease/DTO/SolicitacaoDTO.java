ff222505280868233bf8f3facd6b26e725507b7e
marcelonv011 <marcelonv011@gmail.com>
2023-08-29 11:49:22 -0300

    solicitacao dto alterando

--- src/main/java/com/api/stagease/DTO/SolicitacaoDTO.java
+++ src/main/java/com/api/stagease/DTO/SolicitacaoDTO.java
@@ -17,4 +17,16 @@ public class SolicitacaoDTO {
     private DuracaoEntity duracao;
     private List<Situacao> situacoes;
     private ExpedienteEntity expediente;
+
+    public SolicitacaoDTO() {}
+
+    public SolicitacaoDTO(Long id, LocalEntity local, EstagiarioEntity estagiario, SupervisorEntity supervisor, DuracaoEntity duracao, List<Situacao> situacoes, ExpedienteEntity expediente) {
+        this.id = id;
+        this.local = local;
+        this.estagiario = estagiario;
+        this.supervisor = supervisor;
+        this.duracao = duracao;
+        this.situacoes = situacoes;
+        this.expediente = expediente;
+    }
 }
