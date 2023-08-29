bd5d010aa8a03a11950ca54dd4b5c190c7c44b14
marcelonv011 <marcelonv011@gmail.com>
2023-08-29 11:29:53 -0300

    duracao entity alterando

--- src/main/java/com/api/stagease/Entity/DuracaoEntity.java
+++ src/main/java/com/api/stagease/Entity/DuracaoEntity.java
@@ -1,20 +1,27 @@
 package com.api.stagease.Entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 @Getter
 @Setter
 @Entity
 @Table(name = "duracao", schema = "stagease")
 public class DuracaoEntity extends AbstractEntity{
+    @Column(name = "data_inicio", nullable = false)
+    private LocalTime dataInicio;
+
+    @Column(name = "data_fim", nullable = false)
+    private LocalTime dataFim;
+
+    @Column(name = "carga_horaria", nullable = false)
+    private Long cargaHoraria;
 
     @OneToOne
     @JoinColumn(name = "solicitacao_id")
     private SolicitacaoEntity solicitacao;
-
 }
